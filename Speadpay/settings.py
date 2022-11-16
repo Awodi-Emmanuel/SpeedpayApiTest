9277c097e8997b391fc6dbdaf68d74ee14ec406b
Awodi-Emmanuel <Emmanuelawodi25@gmail.com>
2022-11-17 00:26:06 +0100

    App connected to postgresql successfully

--- Speadpay/settings.py
+++ Speadpay/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'Speadpay.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "speedpayapi",
+        "USER": "speedpay",
+        "PASSWORD": "Speed123_",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
